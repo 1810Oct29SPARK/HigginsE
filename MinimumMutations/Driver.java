a48681b8f23745fd66c5501b45cf5d2b0cd6214a
Jacob Mulrenin <jmulrenin@gmail.com>
2018-12-03 13:31:21 -0500

    minimummutations update

--- MinimumMutations/Driver.java
+++ MinimumMutations/Driver.java
@@ -2,9 +2,6 @@ package com.revature.MinimumMutations;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 public class Driver {
 
@@ -14,26 +11,16 @@ public class Driver {
 		String[] bank = {"AACCGGTA", "AACCGCTA", "AAACGGTA"};
 		System.out.println(mutate.checkMutations("AACCGGTT", "AAACGGTA", bank));
 		
-		/*Reading a file using Scanner
+		/*Reading from a file using Scanner
 		 * File must be in the following format
 		 * AAAAAAAA
 		 * GGGGGGGG
-		 * GGGGGGGG AAAAAAAA CCCCCCCC
+		 * GGGGGGGG AAAAAAAA CCCCCCCC etc. (must be divided by spaces)
 		 */
-		Scanner scan = new Scanner(new File("src/Mutations.txt"));
-		List<String> input =  new ArrayList<String>();
-		while(scan.hasNextLine()) {
-			input.add(scan.nextLine());
-		}
-		scan.close();
-		String[] inputs = new String[3];
-		for(int i = 0; i < input.size(); i++) {
-			inputs[i] = input.get(i);
-		}
-		String start = inputs[0];
-		String end = inputs[1];
-		String[] geneBank = inputs[2].split(" ");
-		System.out.println(mutate.checkMutations(start, end, geneBank));
+		File file1 = new File("src\\Mutations.txt");
+		mutate.readFromFile(file1);
+		File file2 = new File("src\\Mutations2.txt");
+		mutate.readFromFile(file2);
 	}
 
 }
--- MinimumMutations/MinimumMutations.java
+++ MinimumMutations/MinimumMutations.java
@@ -1,5 +1,11 @@
 package com.revature.MinimumMutations;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MinimumMutations {
 	public MinimumMutations() {
 		
@@ -57,28 +63,75 @@ public class MinimumMutations {
 	
 	public int checkMutations(String start, String end, String[] bank) {
 		int count = 0;
+		//if the start or end length is not 8, return -1
 		if(start.length() != 8 || end.length() != 8) {
 			return -1;
-		}else if(start.equals(end)) {
+		}else if(start.equals(end)) { //if start gene = end gene at the start, return 0
 			return 0;
 		}else {
+				//iterate through each string in the string array "bank"
 				for(int k = 0; k < bank.length; k++) {
+					//once the start string equals the end string, break out of the loop
+					if(start.equals(end)) {
+						break;
+					}
+					//ensure each bank string only contains AGTC
+					bank[k] = bank[k].replaceAll("[^AGTC]", "");
+					//if the length of that string after replacing character is not 8, break
+					if(bank[k].length() != 8) {
+						break;
+					}
 					int count2 = 0;
 					int count3 = 0;
+					//iterate through each character of the current bank string
 					for(int l = 0; l < bank[k].length(); l++) {
+						//if the char in bank is the same as the end, but not start, that means it needs to change and add 1 to count2
 						if(bank[k].charAt(l) == end.charAt(l) && bank[k].charAt(l) != start.charAt(l)) {
 							count2++;							
-						}
+						}//if the banks char is the same as the start char, it doesn't need to change and add one to count3
 						if(bank[k].charAt(l) == start.charAt(l)) {
 							count3++;
 						}
 					}
+					//to ensure only 1 char was changed between start and this bank entry
 					if(count2 == 1 && count3 == 7) {
 						start = bank[k];
 						count++;
+						k = -1;
 					}
 				}
 		}
-		return count;
+		//After the previous iteration, ensure that start now equals end, if not print out and error
+		if(start.equals(end)) {
+			return count;
+		}else {
+			System.out.println("end string was not found in the bank or invalid bank entry");
+			return -1;
+		}
+	}
+	
+	public void readFromFile(File filename) throws FileNotFoundException {
+		//create a new scanner that scans "filename"
+		boolean checker = true;
+		Scanner scan = new Scanner(filename);
+		List<String> input =  new ArrayList<String>();
+		//iterate through each line of filename and add it to the ArrayList of strings
+		while(scan.hasNextLine()) {
+			input.add(scan.nextLine());
+		}
+		scan.close();
+		String start = input.get(0);
+		String end = input.get(1);
+		String[] geneBank = input.get(2).split(" ");
+		for(int i = 0; i < geneBank.length; i++) {
+			if(geneBank[i].length() != 8) {
+				checker = false;
+			}
+		}
+		if(checker) {
+			System.out.println(checkMutations(start, end, geneBank));
+		}else {
+			System.out.println("One or more bank entries was invalid");
+		}
 	}
 }
