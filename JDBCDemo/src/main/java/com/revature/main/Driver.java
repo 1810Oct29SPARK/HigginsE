598d52cb2c73fb24217af0d59bd4dafcfaf69b2f
RevatureProtocol <44585462+RevatureProtocol@users.noreply.github.com>
2018-12-07 17:40:01 -0500

    Add files via upload
    Adding JDBCDemo

---
+++ JDBCDemo/src/main/java/com/revature/main/Driver.java
@@ -0,0 +1,32 @@
+package com.revature.main;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.revature.dao.BearDAO;
+import com.revature.dao.BearDAOImpl;
+import com.revature.util.ConnectionUtil;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		BearDAO bd = new BearDAOImpl();
+
+		bd.insertBear(999, "Moby", "11-NOV-95", 200, 2, 2);
+
+	}
+
+	static void init() {
+		try {
+			Connection con = ConnectionUtil.getConnection("connection.properties");
+			System.out.println(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
