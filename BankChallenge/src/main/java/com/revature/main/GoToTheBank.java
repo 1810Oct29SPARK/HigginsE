5afb7e7aa9c65868fe2dba66069ddde6763e5070
Emily Higgins <emilyrchiggins@gmail.com>
2018-12-10 10:31:51 -0500

    more instructions

--- BankChallenge/src/main/java/com/revature/main/GoToTheBank.java
+++ BankChallenge/src/main/java/com/revature/main/GoToTheBank.java
@@ -4,6 +4,7 @@ public class GoToTheBank {
 
 	public static void main(String[] args) {
 	
+		//construct tables in an RDS to support these operations! 
 		//implement BankUtil methods and demonstrate their use in this main method. 
 
 	}
