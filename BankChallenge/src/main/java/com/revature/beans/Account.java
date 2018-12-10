2290fc6df08738d072f9b8bbf2a29d4bd7621573
Max Couch <caxmouch@gmail.com>
2018-12-10 14:49:37 -0500

    TroyAndMax BankChallenge

---
+++ BankChallenge/src/main/java/com/revature/beans/Account.java
@@ -0,0 +1,66 @@
+package com.revature.beans;
+
+public class Account {
+
+	private int id;
+	private short accountNumber;
+	private Customer customer;
+	private String accountName;
+	private Double balance;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public short getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(short accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", accountNumber=" + accountNumber + ", customer=" + customer + ", accountName="
+				+ accountName + ", balance=" + balance + "]";
+	}
+
+	public Account(int id, short accountNumber, Customer customer, String accountName, Double balance) {
+		super();
+		this.id = id;
+		this.accountNumber = accountNumber;
+		this.customer = customer;
+		this.accountName = accountName;
+		this.balance = balance;
+	}
+
+}
---
+++ BankChallenge/src/main/java/com/revature/dao/AccountDAO.java
@@ -0,0 +1,13 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import com.revature.beans.Account;
+
+public interface AccountDAO {
+
+	public Account getAccountById(int id);
+	
+	
+	//define necessary CRUD methods and build an implementing class in this package
+}
