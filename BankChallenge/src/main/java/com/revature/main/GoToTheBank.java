afa2153809de0102f9424a18c4dfb01b6a1d35a7
Emily Higgins <emilyrchiggins@gmail.com>
2018-12-10 10:15:02 -0500

    adding bank challenge skeleton

---
+++ BankChallenge/src/main/java/com/revature/main/GoToTheBank.java
@@ -0,0 +1,10 @@
+package com.revature.main;
+
+public class GoToTheBank {
+
+	public static void main(String[] args) {
+	
+		//implement BankUtil methods and demonstrate their use in this main method. 
+
+	}
+}
