ff3a5bc93b4d7a8cb97c205f267a3e52d7cab91b
Jeremy Shropshire <jeremyshropshire24@gmail.com>
2018-12-17 11:53:17 -0500

    html,css,js code challenge

---
+++ week4/js.js
@@ -0,0 +1,26 @@
+new Vue({ 
+    el: '#cat',
+    data: {
+        image: { url: ""}
+    },
+    created(){
+        this.loadNextImage();
+    } ,
+    methods:{
+        async loadNextImage()
+        {
+            try{
+                axios.defaults.headers.common['x-api-key'] = "DEMO-API-KEY" 
+                let response = await axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:1, size:"full" } } )
+                this.image = response.data[0] 
+                console.log("-- Image from TheCatAPI.com")
+                console.log("id:", this.image.id)
+                console.log("url:", this.image.url)
+            }catch(err){
+                console.log(err)
+            }
+        }
+    }
+})
+
+
