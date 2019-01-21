2922856dd30fc50fa3ff973b0490abcf09797e1c
Kaleigh Jaeger <jaegerka@dukes.jmu.edu>
2019-01-21 12:52:24 -0500

    Initial commit for login portal assignment

---
+++ login-portal/src/main/java/com/revature/loginportal/LoginPortalApplication.java
@@ -0,0 +1,14 @@
+package com.revature.loginportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPortalApplication.class, args);
+	}
+
+}
+
