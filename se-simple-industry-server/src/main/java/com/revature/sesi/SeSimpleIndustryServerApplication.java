fb84d55d69d62528941bf40cbef4a3ffbcf81e30
Eddy Soma <esoma92.dev@gmail.com>
2019-01-21 12:46:10 -0500

    full stack challenge server setup

---
+++ se-simple-industry-server/src/main/java/com/revature/sesi/SeSimpleIndustryServerApplication.java
@@ -0,0 +1,14 @@
+package com.revature.sesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeSimpleIndustryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeSimpleIndustryServerApplication.class, args);
+	}
+
+}
+
---
+++ se-simple-industry-server/src/test/java/com/revature/sesi/SeSimpleIndustryServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.revature.sesi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeSimpleIndustryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
