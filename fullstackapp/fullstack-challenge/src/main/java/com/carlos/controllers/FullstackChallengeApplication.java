cfd94c98fb98621626291827fd8ec8d5cab6a61c
unknown <cbejar93@gmail.com>
2019-01-21 16:13:20 -0500

    cant move the app starter

--- fullstackapp/fullstack-challenge/src/main/java/com/carlos/controllers/FullstackChallengeApplication.java
+++ fullstackapp/fullstack-challenge/src/main/java/com/carlos/controllers/FullstackChallengeApplication.java
@@ -1,4 +1,4 @@
-package com.carlos.fullstackchallenge;
+package com.carlos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
