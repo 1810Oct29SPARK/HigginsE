4092fc6ed241425d85252ce490c6a543b690aed0
sean sulham <sean.sulham@hotmail.com>
2019-02-02 11:09:02 -0500

    Added a class with nested classes

---
+++ ProducerConsumerChallenge/src/main/java/com/revature/main/Main.java
@@ -0,0 +1,27 @@
+package com.revature.main;
+
+public class Main {
+	
+	static class Producer implements Runnable {
+
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	static class Consumer implements Runnable {
+
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	static class Basket {
+		
+	}
+
+}
