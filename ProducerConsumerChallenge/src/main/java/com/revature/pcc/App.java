a9b93e17303ff23fce094fafa1dfbff89171fad6
Eddy Soma <esoma92.dev@gmail.com>
2019-02-04 01:20:34 -0500

    pcc app functional

--- ProducerConsumerChallenge/src/main/java/com/revature/pcc/App.java
+++ ProducerConsumerChallenge/src/main/java/com/revature/pcc/App.java
@@ -2,14 +2,25 @@ package com.revature.pcc;
 
 import org.apache.logging.log4j.*;
 
-import com.revature.pcc.model.*;
-import com.revature.pcc.runnable.*;
 import com.revature.pcc.configuration.*;
 
 public class App {
 
     public static void main(String[] args) {
-		//
+
+		Logger logger = LogManager.getLogger("mainLogger");
+
+		logger.info("\n==================== Welcome to Market Simulator ====================\n");
+
+		MarketConfiguration mc = new MarketConfiguration("market.properties");
+		mc.configure();
+		mc.printConfiguration();
+
+		Market marketSim = mc.buildMarket();
+		marketSim.openMarket();
+
+		logger.info("\n==================== End of Market Simulator ====================\n");
+
     }
 
 }
